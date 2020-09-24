0a39290a032b2c527c8b1a704edcf2456436d592
bala280597 <47313756+bala280597@users.noreply.github.com>
2020-09-24 20:45:59 +0530

    Create db.sql

---
+++ src/db.sql
@@ -0,0 +1,6 @@
+CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'balamurugan@123';
+CREATE DATABASE wikidatabase;  
+GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';
+FLUSH PRIVILEGES;
+SHOW DATABASES;
+SHOW GRANTS FOR 'wiki'@'localhost';
